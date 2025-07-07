c9dc62b1e123924474fa8eda42837a6d811d4db0
cristian770h <cristianlopezrodriguez770@gmail.com>
2025-07-06 23:02:53 -0500

    asd

--- api/routes/leadRoutes.js
+++ api/routes/leadRoutes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { createLead, getLeads,deleteLead,replyLead } = require("../controllers/leadController");
 
-router.post("/leads", createLead);
-router.get("/leads", getLeads);
-router.delete("/leads/:id", deleteLead);
-router.post("/leads/:id/reply", replyLead);
+router.post('/leads', createLead);
+router.get('/leads', getLeads);
+
 
 module.exports = router;
